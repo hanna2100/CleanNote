2e8cb658a3d9abd16eed92fee2edbe42a5dacc50
hanna2100 <hanna0497@naver.com>
2021-02-06 00:06:59 +0900

    네트워크와 캐시 에러핸들링

--- app/src/main/java/com/hanna2100/cleannote/business/interactors/notelist/InsertNewNote.kt
+++ app/src/main/java/com/hanna2100/cleannote/business/interactors/notelist/InsertNewNote.kt
@@ -1,11 +1,15 @@
 package com.hanna2100.cleannote.business.interactors.notelist
 
+import com.hanna2100.cleannote.business.data.cache.CacheResponseHandler
 import com.hanna2100.cleannote.business.data.cache.abstraction.NoteCacheDataSource
 import com.hanna2100.cleannote.business.data.network.abstraction.NoteNetworkDataSource
+import com.hanna2100.cleannote.business.data.util.safeApiCall
+import com.hanna2100.cleannote.business.data.util.safeCacheCall
 import com.hanna2100.cleannote.business.domain.model.Note
 import com.hanna2100.cleannote.business.domain.model.NoteFactory
 import com.hanna2100.cleannote.business.domain.state.*
 import com.hanna2100.cleannote.framwork.presentation.notelist.state.NoteListViewState
+import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import java.util.*
@@ -26,41 +30,49 @@ class InsertNewNote(
             title = title,
             body = ""
         )
-        val cacheResult = noteCacheDataSource.insertNote(newNote)
-        var cacheResponse: DataState<NoteListViewState>? = null
-        if(cacheResult > 0) {
-            val viewState = NoteListViewState(
-                newNote = newNote
-            )
-            cacheResponse = DataState.data(
-                response = Response(
-                    message = INSERT_NOTE_SUCCESS,
-                    uiComponentType = UIComponentType.Toast(),
-                    messageType = MessageType.Success()
-                ),
-                data = viewState,
-                stateEvent = stateEvent
-            )
-        } else {
-            cacheResponse = DataState.data(
-                response = Response(
-                    message = INSERT_NOTE_FAILED,
-                    uiComponentType = UIComponentType.Toast(),
-                    messageType = MessageType.Error()
-                ),
-                data = null,
-                stateEvent = stateEvent
-            )
+        val cacheResult = safeCacheCall(IO) {
+            noteCacheDataSource.insertNote(newNote)
         }
-
+        val cacheResponse = object : CacheResponseHandler<NoteListViewState, Long> (
+            response = cacheResult,
+            stateEvent = stateEvent
+        ) {
+            override fun handleSuccess(result: Long): DataState<NoteListViewState> {
+                return if(result > 0) {
+                    val viewState = NoteListViewState(
+                        newNote = newNote
+                    )
+                    DataState.data(
+                        response = Response(
+                            message = INSERT_NOTE_SUCCESS,
+                            uiComponentType = UIComponentType.Toast(),
+                            messageType = MessageType.Success()
+                        ),
+                        data = viewState,
+                        stateEvent = stateEvent
+                    )
+                } else {
+                    DataState.data(
+                        response = Response(
+                            message = INSERT_NOTE_FAILED,
+                            uiComponentType = UIComponentType.Toast(),
+                            messageType = MessageType.Error()
+                        ),
+                        data = null,
+                        stateEvent = stateEvent
+                    )
+                }
+            }
+        }.getResult()
         emit(cacheResponse)
-        updateNetwork(cacheResponse.stateMessage?.response?.message, newNote)
-
+        updateNetwork(cacheResponse?.stateMessage?.response?.message, newNote)
     }
 
     private suspend fun updateNetwork(cacheResponse: String?, newNote: Note) {
         if (cacheResponse.equals(INSERT_NOTE_SUCCESS)) {
-            noteNetworkDataSource.insertOrUpdateNote(newNote)
+            safeApiCall(IO) {
+                noteNetworkDataSource.insertOrUpdateNote(newNote)
+            }
         }
     }
 
