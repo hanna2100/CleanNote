806f8dbdb412daf1463a207def2964bce8e10a63
hanna2100 <hanna0497@naver.com>
2021-02-08 23:58:58 +0900

    테스트용 fake data 생성

---
+++ app/src/test/java/com/hanna2100/cleannote/business/data/NoteDataFactory.kt
@@ -0,0 +1,37 @@
+package com.hanna2100.cleannote.business.data
+
+import com.google.common.reflect.TypeToken
+import com.google.gson.Gson
+import com.hanna2100.cleannote.business.domain.model.Note
+import java.lang.reflect.Type
+
+class NoteDataFactory(
+    private val testClassLoader: ClassLoader
+) {
+
+    fun produceListOfNotes(): List<Note>{
+        val typeObj: Type = object : TypeToken<List<Note>>() {}.type
+        val notes: List<Note> = Gson().fromJson(
+                json = getNotesFromFile("note_list.json"),
+                Type = typeObj
+            )
+        return notes
+    }
+
+    fun produceHashMapOfNotes(noteList: List<Note>): HashMap<String, Note> {
+        val map = HashMap<String, Note>()
+        for(note in noteList) {
+            map.put(note.id, note)
+        }
+        return map
+    }
+
+    fun produceEmptyListOfNotes(): List<Note>{
+        return ArrayList()
+    }
+
+    fun getNotesFromFile(fileName: String): String {
+        return testClassLoader.getResource(fileName).readText()
+    }
+
+}
--- app/src/test/java/com/hanna2100/cleannote/business/di/DependencyContainer.kt
+++ app/src/test/java/com/hanna2100/cleannote/business/di/DependencyContainer.kt
@@ -1,14 +1,17 @@
 package com.hanna2100.cleannote.business.di
 
+import com.hanna2100.cleannote.business.data.NoteDataFactory
 import com.hanna2100.cleannote.business.data.cache.FakeNoteCacheDataSourceImpl
 import com.hanna2100.cleannote.business.data.cache.abstraction.NoteCacheDataSource
 import com.hanna2100.cleannote.business.data.network.FakeNoteNetworkDataSourceImpl
 import com.hanna2100.cleannote.business.data.network.abstraction.NoteNetworkDataSource
+import com.hanna2100.cleannote.business.domain.model.Note
 import com.hanna2100.cleannote.business.domain.model.NoteFactory
 import com.hanna2100.cleannote.business.domain.util.DateUtil
 import com.hanna2100.cleannote.util.isUnitTest
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.HashMap
 
 class DependencyContainer {
     private val dataFormat = SimpleDateFormat("yyyy-MM-dd hh:mm:ss a", Locale.KOREA)
@@ -16,19 +19,28 @@ class DependencyContainer {
     lateinit var noteNetworkDataSource: NoteNetworkDataSource
     lateinit var noteCacheDataSource: NoteCacheDataSource
     lateinit var noteFactory: NoteFactory
+    lateinit var noteDataFactory: NoteDataFactory
+    lateinit var notesData: HashMap<String, Note>
 
     init {
         isUnitTest =  true
     }
 
     fun build() {
+        this.javaClass.classLoader?.let { classLoader ->
+            noteDataFactory = NoteDataFactory(classLoader)
+            // fake data set
+            notesData = noteDataFactory.produceHashMapOfNotes(
+                noteDataFactory.produceListOfNotes()
+            )
+        }
         noteFactory = NoteFactory(dateUtil)
         noteNetworkDataSource = FakeNoteNetworkDataSourceImpl(
-            notesData = HashMap(),
+            notesData = notesData,
             deletedNotesData = HashMap()
         )
         noteCacheDataSource = FakeNoteCacheDataSourceImpl(
-            notesData = HashMap(),
+            notesData = notesData,
             dateUtil = dateUtil
         )
     }
