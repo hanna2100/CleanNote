07ae1ff998197c2c5c799777b67ae1f546842e00
hanna2100 <hanna0497@naver.com>
2021-03-14 00:56:11 +0900

    gradle, manifest 등 수정

--- app/src/main/java/com/hanna2100/cleannote/framework/presentation/splash/SplashFragment.kt
+++ app/src/main/java/com/hanna2100/cleannote/framework/presentation/splash/SplashFragment.kt
@@ -1,8 +1,12 @@
 package com.hanna2100.cleannote.framework.presentation.splash
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.hanna2100.cleannote.R
+import com.hanna2100.cleannote.framework.presentation.BaseApplication
 import com.hanna2100.cleannote.framework.presentation.common.BaseNoteFragment
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
@@ -11,15 +15,26 @@ import kotlinx.coroutines.FlowPreview
 @ExperimentalCoroutinesApi
 class SplashFragment
 constructor(
-    private val factory: ViewModelProvider.Factory
-) : BaseNoteFragment(R.layout.fragment_splash){
+    private val viewModelFactory: ViewModelProvider.Factory
+): BaseNoteFragment(R.layout.fragment_splash) {
 
-    val viewModel: SplashViewModel by viewModels{
-        factory
+    val viewModel: SplashViewModel by viewModels {
+        viewModelFactory
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navNoteListFragment()
+    }
+
+    private fun navNoteListFragment(){
+        findNavController().navigate(R.id.action_splashFragment_to_noteListFragment)
     }
 
     override fun inject() {
-        TODO("Not yet implemented")
+        activity?.run {
+            (application as BaseApplication).appComponent
+        }?: throw Exception("AppComponent is null.")
     }
 
 }
